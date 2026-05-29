01307a9ce33bcee4400ca281d7dc138e0184decd
Alexander Hosmer <alexanderchosmer@gmail.com>
2026-05-29 12:00:09 -0700

    Created classes, added template code

---
+++ IntegerLinkedList.java
@@ -0,0 +1,27 @@
+
+public class IntegerLinkedList {
+    private IntegerNode head;
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public void insertFront(int val) {
+        // your code here
+    }
+    public void insertBack(int val) {
+        // your code here
+    }
+    public void print() {
+        // your code here
+    }
+    public int get(int index) {
+        // your code here
+    return -1;
+    }
+    public void removeFront() {
+        // your code here
+    }
+    public void removeBack() {
+        // your code here
+    }
+}
+
---
+++ IntegerNode.java
@@ -0,0 +1,27 @@
+public class IntegerNode {
+    private int value;
+    private IntegerNode next;
+    // Constructor to create an empty node
+    public IntegerNode() {}
+    // Constructor to create a node with a specific value
+    public IntegerNode(int val) {
+        this.value = val;
+    }
+    // Getter
+    public int getValue() {
+        return value;
+    }
+    // Setter
+    public void setValue(int val) {
+        this.value = val;
+
+    }
+    // Get the next node
+    public IntegerNode getNext() {
+        return next;
+    }
+    // Set the next node
+    public void setNext(IntegerNode n) {
+        this.next = n;
+    }
+}
